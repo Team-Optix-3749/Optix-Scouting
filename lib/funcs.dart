4edaccc66f230a9c7994a2c3d9d1db00196b34e5
nVarap <nvarap@gmail.com>
2023-01-07 20:37:00 -0800

    Implemented names for different teams

--- lib/funcs.dart
+++ lib/funcs.dart
@@ -4,9 +4,15 @@ import 'package:flutter/services.dart';
 Future<String> getNameFromDB(String number) async {
   //Todo: Set up the API tag
   //Todo: After API setup, start on app:database integration
-  final String response =
-      await rootBundle.loadString('assets/database.json'); // Issue here
-  final data = await json.decode(response);
-  print(data);
-  return "name";
+  try {
+    final String response =
+      await rootBundle.loadString('databases/database.json'); // Issue here
+    final data = await json.decode(response);
+    print(data[number]);
+    return data[number];
+  }
+  catch (Exception){
+    return "Team does not exist";
+  }
+  
 }
