1097670bcc457b42aa53109b6630e5738f9f76eb
ple1ades <jadonjkl@gmail.com>
2023-02-21 17:40:52 -0800

    middle fix

--- lib/pages/match.dart
+++ lib/pages/match.dart
@@ -111,7 +111,7 @@ class _MatchState extends State<Match> {
     Color color = Color.fromARGB(255, 78, 118, 247);
     return Container(
       width: 50,
-      height: 50,
+      height: 60,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
